b6c2e3ae4dd4ac67dc84b58e9feb8fb6bc78dd81
wniy <viniberizzi@gmail.com>
2022-08-26 10:56:31 -0300

    rewrite regex

--- lexema_type.py
+++ lexema_type.py
@@ -1,18 +1,5 @@
 from enum import Enum
 
-class Regexes(Enum):
-    VAR = r'\s*[a-zA-Z]{1}[0-9a-zA-Z_]{0,98}'
-    NUM = r'\s*[1-9]\d{0,98}'
-    LPAR = r'\s*\({1}'
-    RPAR = r'\s*\){1}'
-    ADDOP = r'\s*\+{1}'
-    SUBOP = r'\s*\-{1}'
-    MULOP = r'\s*\*{1}'
-    DIVOP = r'\s*\/{1}'
-    LTOP = r'\s*\>{1}'
-    STOP = r'\s*\<{1}'
-    EQOP = r'\s*\={2}'
-    ASSIGNOP = r'\s*\(\:={1})'
 
 class LexemaType:
     def __init__(self, regex, id):
@@ -20,15 +7,39 @@ class LexemaType:
         self.id = id
 
 class LexemaTypes(Enum):
-    VAR = LexemaType(Regexes.VAR.value, 1)
-    NUM = LexemaType(Regexes.NUM.value, 2)
-    LPAR = LexemaType(Regexes.LPAR.value, 3)
-    RPAR = LexemaType(Regexes.RPAR.value, 4)
-    ADDOP = LexemaType(Regexes.ADDOP.value, 5)
-    SUBOP = LexemaType(Regexes.SUBOP.value, 6)
-    MULOP = LexemaType(Regexes.MULOP.value, 7)
-    DIVOP = LexemaType(Regexes.DIVOP.value, 8)
-    LTOP = LexemaType(Regexes.LTOP.value, 9)
-    STOP = LexemaType(Regexes.STOP.value, 10)
-    EQOP = LexemaType(Regexes.EQOP.value, 11)
-    ASSIGNOP = LexemaType(Regexes.ASSIGNOP.value, 12)
+    VAR = LexemaType(
+        regex = r'\s*[a-zA-Z]{1}[0-9a-zA-Z_]{0,98}',
+        id = 1)
+    NUM = LexemaType(
+        regex = r'\s*[1-9]\d{0,98}',
+        id = 2)
+    LPAR = LexemaType(
+        regex = r'\s*\({1}',
+        id = 3)
+    RPAR = LexemaType(
+        regex = r'\s*\){1}',
+        id = 4)
+    ADDOP = LexemaType(
+        regex = r'\s*\+{1}',
+        id = 5)
+    SUBOP = LexemaType(
+        regex = r'\s*\-{1}',
+        id = 6)
+    MULOP = LexemaType(
+        regex = r'\s*\*{1}',
+        id = 7)
+    DIVOP = LexemaType(
+        regex = r'\s*\/{1}',
+        id = 8)
+    LTOP = LexemaType(
+        regex = r'\s*\>{1}',
+        id = 9)
+    STOP = LexemaType(
+        regex = r'\s*\<{1}',
+        id = 10)
+    EQOP = LexemaType(
+        regex = r'\s*\={2}',
+        id = 11)
+    ASSIGNOP = LexemaType(
+        regex = r'\s*\(\:={1})',
+        id = 12)
