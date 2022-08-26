458f6f8dc45d9d96d0817542fd6db14e7aac7486
wniy <viniberizzi@gmail.com>
2022-08-26 02:37:53 -0300

    refactor

--- tokens.py
+++
@@ -1,8 +0,0 @@
-class Token:
-    def __init__(self, nome, lexema, id):
-        self.nome = nome
-        self.lexema = lexema
-        self.id = id
-
-    def __repr__(self):
-        return '({}, {}, {})'.format(self.nome, self.lexema, self.id)
