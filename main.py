a02a692ec015bbab7c84384a80092fe602627d7f
wniy <viniberizzi@gmail.com>
2022-08-27 00:44:46 -0300

    aaaaaaaaaaaaaaaaaa

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from token_builder import Token
 
-def getNextToken(string):
-    token = Token(string)
-    return token
 
-def trimInput(string, token):
-    length = len(token.lexema)
-    string = string[length:]
-    return string
+def Lexify(inputString):
+    tokens = []
+    while len(inputString) > 0:
+        token = Token()
+        length = token.tokenize(inputString)
+        if not length:
+            break        
+        else:
+            inputString = inputString[length:]
+            tokens.append(token)
 
-test = ") *2"
+    return tokens
 
-tokens = []
-finished = False
-while not finished:
-    token = getNextToken(test)
-    test = trimInput(test, token)
-    tokens.append(token)
+
+def Test(tokens):
+    print(f'\nQuantidade de tokens: {len(tokens)}')
+    print('\nLista de tokens: ')
+    result = ''
+    for token in tokens:
+        print(token)
+        result += token.lexema
+    
+    print('\nInput reconstruído:')
+    print(result)
+
+
+input = "\n\rASDF823gf \n\r\t  *\n\r\t   A\n\r\t   := \n\r\t  0\n\r\t  +\n\r\t  101\n\r\t  -\n\r\t  b9\n\r\t  *\n\r\t  1234\n\r\t  /\n\r\t  2\n\r\t  <\n\r\t  1\n\r\t  >\n\r\t  ==\n\r\t  asdf\n\r\t  "
+
+tokens = Lexify(input)
+Test(tokens)
--- token_builder.py
+++ token_builder.py
@@ -3,17 +3,17 @@ import re
 
 
 class Token:
-    def __init__(self, string):
-        self.tokenize(string)
 
     def tokenize(self, string):
         for lexema_type in LexemaTypes:
             match = re.match(lexema_type.value.regex, string)
             if match:
-                self.lexema = match.group(0).lstrip()
+                content = match.group(0)
+                self.lexema = content.lstrip()
                 self.id = lexema_type.value.id
                 self.name = lexema_type.name
-                break
+                return len(content)
+        return False
 
     def __repr__(self):
         return '({}, \"{}\", {})'.format(self.name, self.lexema, self.id)
